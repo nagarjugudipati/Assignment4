1816611d763c2b1efa62922753c1f847000464dd
nagarajugudipati1@gmail.com <nagarajugudipati1@gmail.com>
2023-07-13 14:49:41 +0530

    Assignment4

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppAssignment4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter a number");
+            num = int.Parse(Console.ReadLine());
+            if(num>0)
+            {
+                Console.WriteLine("The Number is Positive");
+            }
+            else if(num<0)
+            {
+                Console.WriteLine("The Number is Negative");
+            }
+            else
+            {
+                Console.WriteLine("The Number is Zero");
+            }
+            string evenorodd =(num % 2 == 0 )? "The Number isEven" : "The Number is Odd";
+            Console.WriteLine("The number is "+evenorodd+".");
+            Console.ReadKey();
+        }
+    }
+}
